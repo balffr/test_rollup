bea7e43cc17b48805768f0397dfb644dd2c2c868
Ayub <ayub.alibekov@progforce.com>
2020-01-30 12:17:49 +0300

    fix config

--- rollup.config.js
+++ rollup.config.js
@@ -4,21 +4,28 @@ import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 
+import pkg from "./package.json";
+
 const NODE_ENV = process.env.NODE_ENV || "development";
 
 export default {
   input: "src/main.js",
   output: [
     {
-      file: "dist/index.js",
+      file: pkg.main,
       format: "cjs"
     },
     {
-      file: "dist/index.min.js",
-      format: "iife",
-      name: "version",
-      plugins: [terser()]
+      file: pkg.module,
+      format: "es",
+      sourcemap: true
     }
+    // {
+    //   file: "dist/index.min.js",
+    //   format: "iife",
+    //   name: "version",
+    //   plugins: [terser()]
+    // }
   ],
   plugins: [
     replace({
